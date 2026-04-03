a510299d64272c282939207d1457667396338e65
Mallo <barataymallory@gmail.com>
2026-04-03 12:13:08 +0200

    ajout interface a player

--- players/normal_player.py
+++ players/normal_player.py
@@ -1,5 +1,7 @@
+from interface.interface_tk import Interface
 from players.player import Player
 
 class NormalPlayer(Player):
     def __init__(self, name):
         super(NormalPlayer, self).__init__(name)
+        self.interface = Interface()
